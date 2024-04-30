98859ca3f63740aeafc11129c301078b70521a1f
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-04-30 10:19:16 +0300

    add normalize

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="weather"></div>
+</template>
+
+<script>
+export default {
+  name: 'VUEWeatherApp',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
